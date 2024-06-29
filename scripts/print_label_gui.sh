76963f7c6de242eb64268f6c88b2575ad8c0c668
rmantovani68 <rmantovani68@gmail.com>
2024-06-29 17:29:37 +0200

    update

--- scripts/print_label_gui.sh
+++ scripts/print_label_gui.sh
@@ -9,10 +9,11 @@ if zenity --forms --title="Stampa etichetta collo" --text="Inserire dati etichet
   	echo 'processing';
 	data=`cat print_label.dat`;
 	ordine=`echo $data | cut -d "|" -f 1`;
-	sotto_ordine=`echo $data | cut -d "|" -f 2`;
-	collo=`echo $data | cut -d "|" -f 3`;
+	isola=`echo $data | cut -d "|" -f 2`;
+	sotto_ordine=`echo $data | cut -d "|" -f 3`;
 	stampante=`echo $data | cut -d "|" -f 4`;
-	echo cd ../exe; echo ./printlabel -a -o $ordine.$sotto_ordine.$collo -p $stampante;
+	sh -c "cd /u/prj/wella/exe; ./printlabel -a $ordine.$isola.$sotto_ordine -p $stampante";
+	echo sh -c "cd /u/prj/wella/exe; ./printlabel -a $ordine.$isola.$sotto_ordine -p $stampante";
 
   else echo "cancel"
 fi
